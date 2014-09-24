c172812c4a63cae096bb4b4182b8bb54b870e76d
Mike Pol <creoart13@gmail.com>
2014-09-24 11:52:54 +0400

    todo app

--- app.js
+++ app.js
@@ -1,22 +1,37 @@
-'use strict';
-
-TodoApp = angular.module('Todo', []);
+var TodoApp = angular.module('Todo', []);
 
 TodoApp.controller('TodoCtrl', function($scope) {
+	'use strict';
+
 	$scope.todos = [];
 
 	$scope.addTodo = function() {
-		todos.push({
+		if (!$scope.newTodo)
+			return false;
+		$scope.todos.push({
 			"desc": $scope.newTodo,
 			"done": false 
 		});
-	}
+		$scope.newTodo = '';
+	};
+
+	$scope.clearDone = function() {
+		var delTodos = _.where($scope.todos, {done: true});
+		var newTodos = $scope.todos;
+		_.each(delTodos, function(delTodo) {
+			newTodos = _.without(newTodos, delTodo);
+		});
+		$scope.todos = newTodos;
+	};
 
 	$scope.do = function(index) {
-		todos[index].done = true;
-	}
+		if (!$scope.todos[index].done)
+			$scope.todos[index].done = true;
+		else
+			$scope.todos[index].done = false;
+	};
 
 	$scope.delete = function(index) {
-		todos.splice(index, 1);
-	}
+		$scope.todos.splice(index, 1);
+	};
 });
